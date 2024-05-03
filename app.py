84f17282b83e88db5f7f6e09571b615fdd023d47
shaishel <shaishil.maru@gmail.com>
2024-05-03 15:11:06 +0800

    Added Flask + real data

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+from flask import Flask, request
 from woocommerce import API
 import time
 import requests
 import json
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
+
+
+
+# Load environment variables from .env file
 load_dotenv()
-api_key = os.getenv("WOO_CONSUMER_KEY")  
-api_secret = os.getenv("WOO_SECRET_KEY")
-api_lightspeed = os.getenv("LIGTSPEED_KEY")
+
+api_key = os.environ.get("WOO_CONSUMER_KEY")
+api_secret = os.environ.get("WOO_SECRET_KEY")
+api_lightspeed = os.environ.get("LIGHTSPEED_KEY")
 
 # Initialize the WooCommerce API client
 wcapi = API(
@@ -25,65 +31,58 @@ headers = {
     "authorization": f"Bearer {api_lightspeed}"
 }
 
-response = requests.get(url, headers=headers)
-input_json = response.json()
-input_json = input_json["data"]
+app = Flask(__name__)
 
-#     return woocommerce_json
-def convert_to_woocommerce_json(data):
+@app.route("/webhook", methods=["POST"])
+def hook():
+    # Get data from form
+   data = request.form.to_dict()
 
-    woocommerce_json = []
-    if "categories" in data:
-        # Get categories
-        categories_list = data["categories"]
-        for category in categories_list:
-            if "name" in category:
-                woocommerce_category = {
-                    "name": category["name"]
-                }
-                print(woocommerce_category)
-                wcapi.post("products/categories", woocommerce_category).json()
-            else:
-                print("Category doesn't have a name:", category)
-    else:
-        print("No categories found in the input data.")
+    # Print data received
 
-    return woocommerce_json
+   payload_data = json.loads(data["payload"])
+ #  print("Received shaishel ssss:", payload_data)
+    # Extract title, ID, and category
+   title = payload_data["name"]
+ #  product_id = payload_data["id"]
+   category = payload_data["product_type"]["name"]
 
 
-#  convert_to_woocommerce_json(input_json["data"])
+   product_data = {
+        'title': title,
+        'category': category
+   }
 
-product_data = {
-    "category": "5D",
-    "title": "Fintek Braid Line"
-}
+   print('title is ',product_data)
 
-# Check if category exists, if not create one
-category = wcapi.get("products/categories", params={"slug": product_data["category"]}).json()
-if not category:
-    new_category_data = {
-        'name': product_data['category'],
-        'slug': product_data['category'].lower()  # Slug is required
-    }
-    created_category = wcapi.post("products/categories", new_category_data).json()
-    category_id = created_category['id']
-else:
-    category_id = category[0]['id']
 
-# Check if product exists, if not create one
-product = wcapi.get("products", params={"search": product_data["title"]}).json()
+    # Check if category exists, if not create one
+   category = wcapi.get("products/categories", params={"slug": product_data["category"]}).json()
+   if not category:
+        new_category_data = {
+            'name': product_data['category'],
+            'slug': product_data['category'].lower()  # Slug is required
+        }
+        created_category = wcapi.post("products/categories", new_category_data).json()
+        category_id = created_category['id']
+   else:
+        category_id = category[0]['id']
 
-#product = wcapi.get("products/41117").json()
-print('checking...........product\n',product)
-if product:
-    product_id = product[0]['id']
-    # Update existing product's category
-    product_update_data = {
-        'categories': [{'id': category_id}]
-    }
-    updated_product = wcapi.put(f"products/{product_id}", product_update_data).json()
-<<<<<<< HEAD
-    
-=======
-    print("Product updated:", updated_product)
->>>>>>> 4a3da18 (remove comments)
+    # Check if product exists, if not create one
+   product = wcapi.get("products", params={"search": product_data["title"]}).json()
+
+    #product = wcapi.get("products/41117").json()
+   print('checking...........product\n',product)
+   if product:
+        product_id = product[0]['id']
+        # Update existing product's category
+        product_update_data = {
+            'categories': [{'id': category_id}]
+        }
+        updated_product = wcapi.put(f"products/{product_id}", product_update_data).json()
+
+   return product_data
+
+
+if __name__ == "__main__":
+    app.run()
